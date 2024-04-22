dee4bfd8e81cd7b34cd02673ea0eef2dd4682858
manasasanjay <manasanj71@gmail.com>
2024-04-22 14:47:44 +0100

    split dataset by country

--- Data Cleaning Draft.R
+++ Data Cleaning Draft.R
@@ -52,6 +52,44 @@ census_full_1 <- census_full_1[, !names(census_full_1) %in% c("eur")]
 #cannot assign the stupid codes until you split it 
 
 
+#Austria 
+ess1_ata <- ess1[ess1$cntry == "AT", ]
+
+#Switzerland 
+ess1_cha <- ess1[ess1$cntry == "CH", ]
+
+#Czechia 
+ess1_cza <- ess1[ess1$cntry == "CZ", ]
+
+#Denmark 
+ess1_dka <- ess1[ess1$cntry == "DK", ]
+
+#spain 
+ess1_esa <- ess1[ess1$cntry == "ES", ]
+
+#France 
+ess1_fra <- ess1[ess1$cntry == "FR", ]
+
+#Hungary 
+ess1_hua <- ess1[ess1$cntry == "HU", ]
+
+#Ireland 
+ess1_iea <- ess1[ess1$cntry == "IE", ]
+
+#Netherlands 
+ess1_nla <- ess1[ess1$cntry == "NL", ]
+
+#Norway 
+ess1_noa <- ess1[ess1$cntry == "NO", ]
+
+#Poland
+ess1_pla <- ess1[ess1$cntry == "PL", ]
+
+#Portugal
+ess1_pta <- ess1[ess1$cntry == "PT", ]
+
+#Sweden 
+ess1_sea <- ess1[ess1$cntry == "SE", ]
 
 
 
