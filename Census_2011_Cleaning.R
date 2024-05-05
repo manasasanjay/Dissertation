a40881e3f4d644a1a8bbc06f39d903772dafc0b7
manasasanjay <manasanj71@gmail.com>
2024-05-05 11:47:43 +0100

    loaded data for 2011

---
+++ Census_2011_Cleaning.R
@@ -0,0 +1,53 @@
+library(haven)
+library(readxl)
+library(dplyr)
+library(ltm)
+library(corrplot)
+library(ggplot2)
+library(GGally)
+library(ggthemes)
+
+#load 2011 census data for each country 
+austria_11 <- read_excel("Desktop/PPE/DISS/Austria_2011_Census.xlsx")
+
+czechia_11 <- read_excel("Desktop/PPE/DISS/Czechia_2011_Census.xlsx")
+
+denmark_11 <- read_excel("Desktop/PPE/DISS/Denmark_2011_Census.xlsx")
+
+france_11 <- read_excel("Desktop/PPE/DISS/France_2011_Census.xlsx")
+
+ireland_11 <- read_excel("Desktop/PPE/DISS/Ireland_2011_Census.xlsx")
+
+netherlands_11 <- read_excel("Desktop/PPE/DISS/Netherlands_2011_Census.xlsx")
+
+norway_11 <- read_excel("Desktop/PPE/DISS/Norway_2011_Census.xlsx")
+
+poland_11 <- read_excel("Desktop/PPE/DISS/Poland_2011_Census.xlsx")
+
+portugal_11 <- read_excel("Desktop/PPE/DISS/Portugal_2011_Census.xlsx")
+
+spain_11 <- read_excel("Desktop/PPE/DISS/Spain_2011_Census.xlsx")
+
+sweden_11 <- read_excel("Desktop/PPE/DISS/Sweden_2011_Census.xlsx")
+
+switzerland_11 <- read_excel("Desktop/PPE/DISS/Switzerland_2011_Census.xlsx")
+
+census_2011_controls <- read_excel("Desktop/PPE/DISS/2011_Contextual_Controls.xlsx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
